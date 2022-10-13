ce45190b88247ae9f9db25f3c2d2e53782aeaae1
Andressa Da Costa <andressadacostan@gmail.com>
2022-10-13 18:59:40 -0300

    ci: Add publish storybook workflow

---
+++ storybook-static/assets/preview.7ec6eb94.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.70564782.js";import"./iframe.1451ff16.js";import"./es.map.constructor.3dd17a8f.js";import"./es.number.to-fixed.6bba6676.js";import"./index.956cb379.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.7ec6eb94.js.map
